19a8b65e1a507c7e13b66c138c9220179c1a80e0
XIAODI <Xiaodi.Fu@outlook.com>
2025-09-04 11:13:33 -0500

    fxd, batch deletion of dishes

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -10,6 +10,8 @@ import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 @Mapper
 public interface DishMapper {
 
